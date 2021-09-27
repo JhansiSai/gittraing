ac5360f20192ae68a497c3861b251b006247d6c6
JhansiSai <jhansisaipallerla@gmail.com>
2021-09-27 13:49:42 +0530

    'initial'

--- backup/screens/Home/HomeContainer.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { Button, Center, NativeBaseProvider } from "native-base"
-export default HomeContainer = () => {
-  return <Button onPress={() => console.log("hello world")}>PRIMARY</Button>
-}
--- backup/screens/MyPractice.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider, Box, Center } from 'native-base';
-
-export default function MyPractice() {
-    return (
-      <Center>Hello World</Center>
-    );
-  }
